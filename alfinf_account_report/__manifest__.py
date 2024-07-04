a8fe3aeacf2e37a5e68d34a2ea8b3efd146d6fea
Pablo Martín López <81068583+pablomil2000@users.noreply.github.com>
2024-07-04 10:21:01 +0200

    feat: Add Alfinf Almagra account report module
    This commit adds the Alfinf Almagra account report module, which includes the necessary files and dependencies for generating custom invoice documents.

---
+++ alfinf_account_report/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 Pablo Martín - Alfinf
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Alfinf Almagra account report",
+    "summary": "Alfinf Almagra account report",
+    "version": "16.0.1.0.0",
+    "development_status": "Beta",
+    "license": "AGPL-3",
+    "author": "Alfinf",
+    "website": "https://github.com/alfinfsi/",
+    "category": "Connector",
+    "depends": [
+        'account'
+    ],
+    "data": [
+        'views/invoice_template.xml'
+    ],
+    "installable": True,
+}
