7202c508ef39f399745a9aaf550a998c78c20f4d
Pablo Martín López <81068583+pablomil2000@users.noreply.github.com>
2024-07-22 12:21:36 +0200

    feat: Renombrar módulo de informe de cuenta Alfinf a informe de contrato

--- alfinf_report_contract/__manifest__.py
+++ alfinf_report_contract/__manifest__.py
@@ -2,8 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Alfinf Almagra account report",
-    "summary": "Alfinf Almagra account report",
+    "name": "Alfinf report contract",
+    "summary": "Alfinf report contract",
     "version": "16.0.1.0.0",
     "development_status": "Beta",
     "license": "AGPL-3",
@@ -11,11 +11,11 @@
     "website": "https://github.com/alfinfsi/",
     "category": "Connector",
     "depends": [
-        'account'
     ],
     "data": [
         "report/ir_actions_report_templates.xml",
         "report/ir_actions_report.xml",
+
     ],
     "installable": True,
 }
